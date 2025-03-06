dabc79d3d5dedf8e40f6576fafff7b1573baa649
NineDayCC <lixvwei@gmail.com>
2025-03-06 15:43:19 +0800

    修正中文链接，统一URL格式

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -22,7 +22,7 @@ html_theme_options['icon_links'] = [
         },
         {
             "name": "中文",
-            "url": "https://docs.nineday.cc/projects/headtracker-esp32/zh_CN/latest/index.html",
+            "url": "https://docs.nineday.cc/projects/headtracker-esp32/zh-cn/latest/index.html",
             "icon": "fa-solid fa-language",
             "type": "fontawesome",
             "attributes": {
